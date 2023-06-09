73a2d8015c0b45d1f2fe9ff55c08d99ad25d82b0
StudentAidyn <AIES215175@gmail.com>
2023-06-09 16:12:53 +1000

    01_Initial-Default

---
+++ I2Cpp_Task3/I2Cpp_Task3/I2Cpp_Task3.cpp
@@ -0,0 +1,70 @@
+// I2Cpp_Task3.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
+
+
+//Problem Statements
+//
+//This information will help guide your response to item four(4).
+//
+//Problem statements summarize key information about the problemand the 
+//environment surrounding it.They are often brief, but can provide detail 
+//as necessary to describe the problem.
+//
+//Consider this sample problem statement written for game engines :
+//
+//Small development teams may find it expensive to split their resources 
+//between building the game engine vs.building the actual game itself.
+//Teams may need to dedicate at least 1 - 2 full - time employees(FTEs) 
+//to developing the engine as requirements change over the product lifecycle, 
+//increasing operational costs.
+//
+//Developers may choose to use game engines middleware like Unity or Unreal Engine 
+//in exchange for a small upfront cost in the form of licenses or royalties.
+//Engineers can be instead be dedicated to developing the game instead, 
+//requiring less engineersand thus reducing operational costs.
+//
+//Game engine middleware is often maintained by teams with dedicated R& D
+//teams, providing its users with the latest innovations in the games industry 
+//at a fraction of the cost.Teams may hire for users experienced in specific 
+//game engines, minimizing resources spent on trainingand ramp - up.
+//
+//If a game engine does not providing the features required for a particular 
+//game, its cost savings are reduced.Furthermore, missing features or bugs with 
+//the feature set can be difficult to fix if source access is not providing,
+//possibly making the game engine more expensive to work with than a custom 
+//built one.
+//
+//
+//
+//In a few sentences, the passage of text describe the following :
+//
+//What is the problem ? – The cost of building and maintaining custom game engines
+//Who is experiencing the problem ? – Small or new game development teams
+//What is needed in a solution to this problem ? – Minimizes the game engine development costs
+//The solution, game engines middleware like Unity or Unreal Engine, are described as follows :
+//
+//How does Unity or Unreal solve this problem ? – Licenses or royalties for the game 
+//engine can be much more affordable than building a custom engine
+// 
+//What are its advantages ?
+// 
+//Externalized cost of R & D teams
+//New hires may require less training and on - boarding to be effective due to common
+//knowledge of the game engine
+// 
+//What are its disadvantages?
+// 
+//Missing or incomplete features can require teams to devote some or all of the cost 
+//of developing those features in - house regardless
+// 
+//Bugs in the game engine may not be readily diagnosed fixed if source - access is not provided
+
+//END of GIT TESTER
