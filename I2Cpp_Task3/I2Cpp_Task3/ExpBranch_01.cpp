6e2bcb0313d1ee4df69721b06a9b53813affe384
StudentAidyn <AIES215175@gmail.com>
2023-06-13 12:50:35 +1000

    Experiment 1 of exp-branch 01

---
+++ I2Cpp_Task3/I2Cpp_Task3/ExpBranch_01.cpp
@@ -0,0 +1,35 @@
+#include "ExpBranch_01.h"
+
+//THIS IS EXPERIMENT 01
+
+//Problem Statements
+//
+//This information will help guide your response to item four(4).
+//
+//Problem statements summarize key information about the problemand the 
+//environment surrounding it.They are often brief, but can provide detail 
+//as necessary to describe the problem.
+//
+//Consider this sample problem statement written for game engines :
+//
+//Small development teams may find it expensive to split their resources 
+//between building the game engine vs.building the actual game itself.
+//Teams may need to dedicate at least 1 - 2 full - time employees(FTEs) 
+//to developing the engine as requirements change over the product lifecycle, 
+//increasing operational costs.
+//
+//Developers may choose to use game engines middleware like Unity or Unreal Engine 
+//in exchange for a small upfront cost in the form of licenses or royalties.
+//Engineers can be instead be dedicated to developing the game instead, 
+//requiring less engineersand thus reducing operational costs.
+//
+//Game engine middleware is often maintained by teams with dedicated R& D
+//teams, providing its users with the latest innovations in the games industry 
+//at a fraction of the cost.Teams may hire for users experienced in specific 
+//game engines, minimizing resources spent on trainingand ramp - up.
+//
+//If a game engine does not providing the features required for a particular 
+//game, its cost savings are reduced.Furthermore, missing features or bugs with 
+//the feature set can be difficult to fix if source access is not providing,
+//possibly making the game engine more expensive to work with than a custom 
+//built one.
